a9450b4df8eef8b15292ca7b5dae56b3cf29f5ac
Azim Izzudin Mubarak <azimizzudinr@outlook.com>
2023-07-31 11:28:13 +0200

    03-02

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_exercise/loginPage.dart';
 
 void main() {
   runApp(ChatApp());
@@ -12,22 +13,6 @@ class ChatApp extends StatelessWidget {
       title: 'Flutter Chat App',
       theme: ThemeData(primarySwatch: Colors.red),
       home: const LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: const Drawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => print('Button clicked'),
-      ),
-      body: const Text('Hello World'),
     );
   }
 }
